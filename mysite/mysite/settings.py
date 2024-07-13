c579b490b249a7cbb5bee85e545255957bfb0b5b
Mindaugas66 <mindaugas.bog@gmail.com>
2024-07-13 18:26:54 +0300

    updated login/password reset pages,

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-i=d#jm+k(g^#$7-hvxeg75k4puq-n@zs98mp4mys6ud1*+e*6z
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -117,11 +115,12 @@ LOGGING = {
     },
 }
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / "inventory" / "static",
+    BASE_DIR / "inventory/static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
@@ -133,7 +132,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
